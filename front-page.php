1cb6e13c2a1f1fa16da38c8876ff05b6cf8e1a20
allyson_s <allysonsmithstudio@gmail.com>
2022-03-21 11:36:56 -0400

    Initial commit

---
+++ front-page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for the homepage
+ *
+ * @package WordPress
+ * @subpackage Skillcrush_Starter
+ * @since Skillcrush Starter 2.0
+ */
+
+get_header(); ?>
+
+<section class="home-page">
+	<div class="main-content">
+		<div class="content">
+			<?php while ( have_posts() ): the_post(); ?>
+				<h1><?php the_title(); ?></h1>
+				<?php the_content(); ?>
+				<div class="social-btns">  <!-- populate or remove as many of these as you want -->
+					<a href="https://www.linkedin.com/in/allyson-smith-0a808126/" class="soc-icon ln"><span class="screen-reader-text">LinkedIn</span></a>
+					<a href="https://github.com/allyson-s-code/" class="soc-icon gh"><span class="screen-reader-text">GitHub</span></a>
+					<a href="https://www.instagram.com/allysonsmithdev/" class="soc-icon in"><span class="screen-reader-text">Instagram</span></a>
+				</div>
+				<a href="<?php echo site_url('/blog/'); ?>" class="btn">View My Blog</a>
+			<?php endwhile; ?>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
